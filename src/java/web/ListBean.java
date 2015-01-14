19fceb45f64a513b46758358a33374953e35a1e8
Michał Niewiarowski <niewiar@gmail.com>
2015-01-14 21:48:31 +0100

    dodawanie kontrahentów

--- src/java/web/ListBean.java
+++ src/java/web/ListBean.java
@@ -5,8 +5,13 @@
 package web;
 
 import entities.Client;
+import entities.Groups;
+import entities.Users;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,6 +20,7 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletContext;
 import jpa.ClientJpaController;
+import jpa.GroupsJpaController;
 
 /**
  *
@@ -26,7 +32,8 @@ public class ListBean {
 
     private List<Client> clients = new ArrayList<Client>();
     private ClientJpaController cjc = new ClientJpaController();
-
+    private Client newClient = new Client();
+    
     public List<Client> getClients() {
         clients = cjc.findClientEntities();
         return clients;
@@ -35,7 +42,25 @@ public class ListBean {
     public void setClients(List<Client> clients) {
         this.clients = clients;
     }
+    
+     public Client getNewClient() {
+        return newClient;
+    }
 
+    public void setNewClient(Client newClient) {
+        this.newClient = newClient;
+    }
+
+    public void createClient() {
+        try {
+            cjc.create(newClient);
+        } catch (Exception ex) {
+            Logger.getLogger(ListBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        
+        
+    
     public void goToDetails(int id) {
         ServletContext servletContext = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
         String appCtx = servletContext.getContextPath();
